3de66b3c64170d2d5509066b575fafb444c9805d
SANJANAALSHI <61967838+SANJANAALSHI@users.noreply.github.com>
2021-03-13 20:58:19 +0530

    n
    n

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+    result = document.getElementById("result");
+    result.innerHTML = "If you deposit " + principal + "," +
+"at an interest rate of " + rate +
+"You will receive an amount of " + (principal * years * rate / 100) +"," +
+"in the year " + (2021+years);
     
 }
         
